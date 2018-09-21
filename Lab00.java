d78bce03af3b95f7bfac9abd341423f8d3f93ca9
Simon Ibssa <ibssasimon@gmail.com>
2018-09-21 12:53:56 -0700

    testing github functionality

--- Lab00.java
+++ Lab00.java
@@ -1,8 +1,5 @@
-public class Lab00
-{
-   public static void main(String[] args)
-   {
-
-   
+public class Lab00 {
+   public static void main(String[] args) {
+      System.out.println("Hi");
    }
 }
