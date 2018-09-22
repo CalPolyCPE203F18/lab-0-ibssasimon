5125437597b86200ea7a2480551222923abe1464
Simon Ibssa <ibssasimon@gmail.com>
2018-09-22 13:12:47 -0700

    Completed Lab

--- Lab00.java
+++ Lab00.java
@@ -1,5 +1,44 @@
+/**
+ * Lab 00 submission for Simon Ibssa
+ * CPE 203, Julie Workman
+ */
 public class Lab00 {
-   public static void main(String[] args) {
-      System.out.println("Hi");
-   }
+
+  public static void main (String[] args) {
+    // declaring and initializing some variables
+    int x = 5;
+    String y = "hello";
+    double z = 9.8;
+
+    // printing the variables
+    System.out.println("x: " + x + " " + "y: " + y + " " + "z: " + z);
+
+    // a list (make an array in java)
+    int[] nums = {3, 6, -1, 2};
+    for(int num : nums) {
+      System.out.println(num);
+    }
+
+    int numFound = char_count(y,'l');
+    System.out.println("Found: " + numFound);
+
+    // a counting for loop
+    for(int i = 1; i < 11; i++) {
+      System.out.print(i + " ");
+    }
+    System.out.print("\n");
+  }
+
+  private static int char_count(String y, char target) {
+    int count = 0;
+    int asciiVal = target;
+
+    for(int i = 0; i < y.length() - 1; i++) {
+      if(y.charAt(i) == asciiVal) {
+        count++;
+      }
+    }
+    return count;
+  }
+
 }
